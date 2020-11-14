d79cd463b1e8cb7cd69d536e769c0d32e80e41df
chop1k <fakeshopendesu@gmail.com>
2020-11-14 19:18:26 +0300

    added comments

--- src/Consolly.php
+++ src/Consolly.php
@@ -56,6 +56,27 @@ class Consolly
         $this->defaultCommand = null;
     }
 
+    /**
+     * Handles commands by given args
+     *
+     * @param array $args
+     * Console arguments
+     *
+     * @param bool $ignoreFirst
+     * If true first item of array will be ignored
+     *
+     * @throws CommandNotFoundException
+     * Throws when command not found and default command not defined
+     *
+     * @throws NameConflictException
+     * Throws when option with same name found
+     *
+     * @throws OptionRequiredException
+     * Throws when required option of command not defined
+     *
+     * @throws OptionRequiresValueException
+     * Throws when option requires value and value not specified
+     */
     public function handle(array $args, $ignoreFirst = true): void
     {
         if ($ignoreFirst)
@@ -82,6 +103,16 @@ class Consolly
         $this->handleCommand($command, array_slice($args, 0, $i), array_slice($args, $i+1));
     }
 
+    /**
+     * Search command in given args
+     *
+     * @param array $args
+     * Array of argument
+     *
+     * @return array
+     * Array wherein first item is command position in args, second item is command instance.
+     * If command not found it returns [0, null]
+     */
     private function getCommand(array $args): array
     {
         for ($i = 0; $i < count($args); $i++)
@@ -100,6 +131,21 @@ class Consolly
         return [0, null];
     }
 
+    /**
+     * Handles given options
+     *
+     * @param array $args
+     * Array of all args
+     *
+     * @param array $options
+     * Array of command options
+     *
+     * @return int
+     * Number of handled required options for comparison
+     *
+     * @throws OptionRequiresValueException
+     * Throws when option requires value and value not specified
+     */
     private function handleOptions(array $args, array $options): int
     {
         $handledRequiredOptions = 0;
@@ -155,6 +201,30 @@ class Consolly
         return $handledRequiredOptions;
     }
 
+    /**
+     * Gets option and handles it
+     *
+     * @param array $args
+     * Array of args
+     *
+     * @param array $options
+     * Array of command options
+     *
+     * @param string $option
+     * Option name
+     *
+     * @param int $index
+     * Next argument index
+     *
+     * @param int $argsCount
+     * Number of arguments
+     *
+     * @return array|false[]
+     * Returns array wherein first item is option isRequired property, second item is isRequiresValue property
+     *
+     * @throws OptionRequiresValueException
+     * Throws when option requires value and value not specified
+     */
     private function processOption(array $args, array $options, string $option, int $index, int $argsCount): array
     {
         $option = $this->getOption($options, $option);
@@ -169,11 +239,35 @@ class Consolly
         return [$option->isRequired(), $option->isRequiresValue()];
     }
 
+    /**
+     * Shortcut
+     *
+     * @param array $options
+     * Array of command options
+     *
+     * @param string $option
+     * Option name
+     *
+     * @return Option|null
+     * Returns option if exists otherwise returns null
+     */
     private function getOption(array $options, string $option): ?Option
     {
         return isset($options[$option]) ? $options[$option] : null;
     }
 
+    /**
+     * Handles option
+     *
+     * @param Option $option
+     * Option instance
+     *
+     * @param Argument|null $nextArg
+     * Next argument which will be option value if option requires it
+     *
+     * @throws OptionRequiresValueException
+     * Throws when option requires value and value not specified
+     */
     private function handleOption(Option $option, ?Argument $nextArg): void
     {
         if ($option->isRequiresValue())
@@ -194,6 +288,18 @@ class Consolly
         $option->setIndicated(true);
     }
 
+    /**
+     * Handles command options, sets aliases and counts number of required options
+     *
+     * @param Command $command
+     * Command instance
+     *
+     * @return array
+     * Returns array wherein first item is number of required options, second item is array of handled options
+     *
+     * @throws NameConflictException
+     * Throws when option with same name found
+     */
     private function getOptions(Command $command): array
     {
         $options = [];
@@ -232,6 +338,27 @@ class Consolly
         return [$requiredOptions, $options];
     }
 
+    /**
+     * Handles command and executes command handler
+     *
+     * @param Command $command
+     * Command instance
+     *
+     * @param array $args
+     * Array of console arguments preceding the command
+     *
+     * @param array $nextArgs
+     * Array of console arguments following the command
+     *
+     * @throws NameConflictException
+     * Throws when option with same name found
+     *
+     * @throws OptionRequiredException
+     * Throws when option requires value and value not specified
+     *
+     * @throws OptionRequiresValueException
+     * Throws when option requires value and value not specified
+     */
     private function handleCommand(Command $command, array $args, array $nextArgs): void
     {
         [$requiredOptions, $options] = $this->getOptions($command);
